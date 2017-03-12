c6d39721fb7c24af746d9830fbd9fd98658c843a
Nico Gris <riesco@gmail.com>
2017-03-12 00:32:52 -0300

    formatting

--- js/ferns.js
+++ js/ferns.js
@@ -1,8 +1,9 @@
-(function (){
+(function() {
   var VIZ = {};
-  var width = 2, height = 2;
-  var basePoint = {x: 0, y: 0};
-  var colors = ['#006600','#663333','#CC0033','#330099'];
+  var width = 3,
+    height = 3;
+  var basePoint = { x: 0, y: 0 };
+  var colors = ['#006600', '#663333', '#CC0033', '#330099'];
   var svg = d3.select("#svg-container")
     .append("svg")
     .attr("id", "thesvg")
@@ -15,11 +16,10 @@
   VIZ.count = 0;
 
   function getCoords(x, y) {
-    var p = random(1,1000);
-    return p <= 701 ? {c: 0, x: 0.81 * x + 0.07  * y + 0.12, y: -0.04 * x + 0.84 * y + 0.195}: 
-           p <= 851 ? {c: 1, x: 0.18 * x - 0.25  * y + 0.12, y: 0.27  * x + 0.23 * y + 0.02 }:
-           p <= 980 ? {c: 2, x: 0.19 * x + 0.275 * y + 0.16, y: 0.238 * x - 0.14 * y + 0.12 }:
-           {c: 3, x: 0.0235 * x + 0.087 * y + 0.11, y: 0.045 * x + 0.1666 * y};
+    var p = random(1, 1000);
+    return p <= 701 ? { c: 0, x: 0.81 * x + 0.07 * y + 0.12, y: -0.04 * x + 0.84 * y + 0.195 } :
+      p <= 851 ? { c: 1, x: 0.18 * x - 0.25 * y + 0.12, y: 0.27 * x + 0.23 * y + 0.02 } :
+      p <= 980 ? { c: 2, x: 0.19 * x + 0.275 * y + 0.16, y: 0.238 * x - 0.14 * y + 0.12 } : { c: 3, x: 0.0235 * x + 0.087 * y + 0.11, y: 0.045 * x + 0.1666 * y };
   }
 
   function random(min, max) {
@@ -29,30 +29,39 @@
   function renderPoint(data, flag) {
     svg.append("circle")
       .attr("class", "fractalPoint")
-      .style("fill", flag ? colors[0]: colors[data.c])
-      .style("opacity", .6)
+      .style("fill", flag ? colors[0] : colors[data.color])
+      .style("opacity", 0.6)
       .attr("cx", data.x)
       .attr("cy", data.y)
-      .attr("r", .002)
+      .attr("r", 0.002);
   }
 
-  VIZ.addPoint = function (colors) {
+  VIZ.addPoint = function(colors) {
     var xy = getCoords(basePoint.x, basePoint.y);
     basePoint = xy;
     if (colors) {
-      renderPoint({c: xy.c, x: xy.x + width / 5, y: xy.y + height / 10}, 1);
+      renderPoint({
+        color: xy.c,
+        x: basePoint.x + (width / 5),
+        y: basePoint.y + (height / 10)
+      }, 1);
     } else {
-      renderPoint({c: xy.c, x: xy.x + width / 5, y: xy.y + height / 10}, 0);
+      renderPoint({
+        color: xy.c,
+        x: basePoint.x + (width / 5),
+        y: basePoint.y + (height / 10)
+      }, 0);
     }
-  }
+  };
 
-  VIZ.onResize = function () {
-    var aspect = height / width, chart = $("#thesvg");
+  VIZ.onResize = function() {
+    var aspect = height / width,
+      chart = $("#thesvg");
     var targetWidth = chart.parent().width();
     chart.attr("width", targetWidth);
     chart.attr("height", targetWidth / aspect);
-  }
+  };
 
   window.VIZ = VIZ;
 
-}())
+}());
